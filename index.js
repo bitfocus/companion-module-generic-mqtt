6fd47edaa2f6804ff67cffb87f4b164ace62e5d0
Matt Snyder <matt@obscurelogic.com>
2020-02-03 21:24:33 -0600

    Dynamic variable definition via feedback configuration.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var instance_skel = require('../../instance_skel');
 var mqtt = require("mqtt");
-var client;
+var match = require("mqtt-match");
+var mqttClient;
 var debug;
 var log;
 
@@ -10,7 +11,11 @@ class instance extends instance_skel {
 
 		var self = this;
 
+		self.system = system;
+		self.mqtt_topic_subscriptions = new Map();
+
 		self.actions(); // export actions
+
 	}
 
 	updateConfig(config) {
@@ -19,7 +24,7 @@ class instance extends instance_skel {
 		self.config = config;
 
 		self.actions();
-		self._connectMqtt()
+		self._initMqtt();
 	}
 
 	init() {
@@ -28,8 +33,48 @@ class instance extends instance_skel {
 		debug = self.debug;
 		log = self.log;
 
-		self._connectMqtt()
+		self._initMqtt();
+		self._initFeedbackDefinitions();
+		self._updateInstanceFeedbacks();
 
+		// Watch for feedback delete so MQTT subscriptions can be updated.
+		self.system.on('feedback_delete', function (page, bank, id) {
+			debug('Feedback delete event received.');
+			self._unsubscribeAll();
+			self._clearInstanceVariables();
+			self._updateInstanceFeedbacks();
+		});
+
+
+	}
+
+	_initFeedbackDefinitions() {
+		var self = this;
+
+		self.setFeedbackDefinitions({
+			mqtt_variable: {
+				label: 'Update variable with value from MQTT topic',
+				description: 'Receive messages from the MQTT broker and set the value to a variable. Variables can be used on any button.',
+				options: [
+					{
+						type: 'textinput',
+						label: 'Topic',
+						id: 'subscribeTopic',
+						default: ''
+					},
+					{
+						type: 'textinput',
+						label: 'Variable',
+						id: 'variable',
+						default: ''
+					}
+				],
+				callback: (feedback) => {
+					debug('Feedback callback called');
+					self._addMqttFeedback(feedback)
+				}
+			}
+		});
 	}
 
 	config_fields() {
@@ -43,10 +88,10 @@ class instance extends instance_skel {
 				width: 4,
 				default: 1,
 				choices: [
-					{ id: 'mqtt://', label: 'mqtt://' },
-					{ id: 'mqtts://', label: 'mqtts://' },
-					{ id: 'ws://', label: 'ws://' },
-					{ id: 'wss://', label: 'wss://'}
+					{id: 'mqtt://', label: 'mqtt://'},
+					{id: 'mqtts://', label: 'mqtts://'},
+					{id: 'ws://', label: 'ws://'},
+					{id: 'wss://', label: 'wss://'}
 				]
 			},
 			{
@@ -74,13 +119,13 @@ class instance extends instance_skel {
 				id: 'password',
 				width: 6,
 				label: 'Password'
-			},
+			}
 		];
 	}
 
 	destroy() {
-		if(client && client.connected) {
-			client.disconnect()
+		if (mqttClient && mqttClient.connected) {
+			mqttClient.disconnect()
 		}
 	}
 
@@ -134,27 +179,68 @@ class instance extends instance_skel {
 		}
 	}
 
-	_connectMqtt() {
+	_addMqttFeedback(feedback, bank) {
 		var self = this;
 
-		debug('MQTT protocol', self.config.protocol);
+		debug("Adding MQTT feedback");
+		debug(feedback);
 
-		client = mqtt.connect(self.config.protocol + self.config.broker_ip, {username: self.config.user, password: self.config.password})
+		if(feedback.options.subscribeTopic === undefined || feedback.options.subscribeTopic === '') {
+			debug('Skipping empty MQTT feedback topic.');
+			return;
+		}
 
-		client.on('connect', function() {
+		// TODO switch case feedback type if more are added since callback will be specific to the type
+
+		var obj = {
+			topic: feedback.options.subscribeTopic,
+			options: feedback.options,
+			callback: (variable, mqttMessage) => {
+				debug(`Setting value for instance variable: ${variable}`);
+
+				// TODO extract value from field if object path provided.
+
+				// Update variable value for this feedback when message is received.
+				self.setVariable(variable, mqttMessage)
+			}
+		};
+
+		debug(obj);
+
+		self._unsubscribeAll();
+		self._clearInstanceVariables();
+		self.mqtt_topic_subscriptions.set(feedback.id, obj);
+
+		self._refreshMqttSubscriptions();
+		self._updateInstanceVariables();
+	}
+
+	_initMqtt() {
+		var self = this;
+
+		mqttClient = mqtt.connect(self.config.protocol + self.config.broker_ip, {
+			username: self.config.user,
+			password: self.config.password
+		});
+
+		mqttClient.on('connect', () => {
 			self.status(self.STATUS_OK)
 		});
 
-		client.on('error', function(error) {
+		mqttClient.on('error', error => {
 			self.status(self.STATUS_ERROR, error)
 		});
 
-		client.on('offline', function() {
+		mqttClient.on('offline', () => {
 			self.status(self.STATUS_WARNING, 'Offline')
 		});
 
-		client.on('packetreceive', function(packet) {
+		mqttClient.on('packetreceive', packet => {
 			debug('MQTT', packet)
+		});
+
+		mqttClient.on('message', function(topic, message, packet) {
+			self._handleMqttMessage(topic, message, packet)
 		})
 	}
 
@@ -162,12 +248,114 @@ class instance extends instance_skel {
 		var self = this;
 
 		debug('Sending MQTT message', [topic, payload]);
-		if (!client.connected) {
-			self.status(self.STATUS_WARNING, 'Offline')
-			self._connectMqtt()
-		}
 
-		client.publish(topic, payload, {qos: qos, retain: retain})
+		self._reconnectMqtt();
+
+		mqttClient.publish(topic, payload, {qos: qos, retain: retain})
+	}
+
+	_refreshMqttSubscriptions() {
+		var self = this;
+
+		debug('Refreshing MQTT subscriptions: ', self.mqtt_topic_subscriptions);
+
+		self.mqtt_topic_subscriptions.forEach((obj, index) => {
+			mqttClient.subscribe(obj.topic, (err) => {
+				if (!err) {
+					debug(`Successfully subscribed to topic: ${obj.topic}`)
+					return;
+				}
+
+				debug(`Failed to subscribe to topic: ${obj.topic}. Error: ${err}`)
+			})
+		})
+	}
+
+	_reconnectMqtt() {
+		var self = this;
+
+		if (!mqttClient.connected) {
+			mqttClient.reconnect();
+
+			if (!mqttClient.connected) {
+				self.status(self.STATUS_WARNING, 'Offline')
+			}
+		}
+	}
+
+	_handleMqttMessage(topic, message, packet) {
+		var self = this;
+
+		debug('MQTT message received:', {
+			topic: topic,
+			message: message.toString()
+		});
+
+		debug(self.mqtt_topic_subscriptions);
+
+		// Match topic to correct subscriber
+		var sub = self._getSubscriptionsByTopic(topic);
+
+		sub.forEach(function(s) {
+			debug('Matching subscription: ', s);
+			s.callback(s.options.variable, message.toString());
+		});
+	}
+
+	_getSubscriptionsByTopic(topic) {
+		var self = this;
+
+		return [...self.mqtt_topic_subscriptions.values()].filter((v) => {
+			return match(topic, v.topic)
+		});
+	}
+
+	_updateInstanceFeedbacks() {
+		var self = this;
+
+		self.system.emit('feedbacks_for_instance', self.id, function (feedbacks) {
+			feedbacks.forEach((feedback) => {
+				debug('Initializing existing MQTT feedbacks.');
+				self._addMqttFeedback(feedback);
+			})
+		})
+	}
+
+	_updateInstanceVariables() {
+		var self = this;
+
+		var vars = [...self.mqtt_topic_subscriptions.values()].map(function(sub) {
+			debug(sub);
+			return {
+				label: `MQTT value from topic: ${sub.topic}`,
+				name: sub.options.variable,
+			}
+		});
+
+		debug('Refreshing variable definitions:', vars);
+		self.setVariableDefinitions(vars)
+	}
+
+	_unsubscribeAll() {
+		var self = this;
+
+		// This is necessary to prevent orphaned subscriptions when a feedback is updated.
+
+		self.mqtt_topic_subscriptions.forEach((obj, index) => {
+			mqttClient.unsubscribe(obj.topic, (err) => {
+				if (!err) {
+					debug(`Successfully unsubscribed from topic: ${obj.topic}`)
+					return;
+				}
+
+				debug(`Failed to unsubscribe from topic: ${obj.topic}. Error: ${err}`)
+			})
+		});
+	}
+
+	_clearInstanceVariables() {
+		var self = this;
+		self.setVariableDefinitions([]);
 	}
 }
 
